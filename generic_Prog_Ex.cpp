9c3e01606fe412e1c3e038accc5f8cd675d384a0
Mohit Kumar <39983059+anodit@users.noreply.github.com>
2020-06-09 21:34:07 +0530

    first prog

---
+++ generic_Prog_Ex.cpp
@@ -0,0 +1,46 @@
+//Prog without using Generic Programming.
+#include<iostream>
+using namespace std;
+
+int search(int a[],int n,int key)
+{
+    for(int p=0;p<n;p++)
+    {
+        if(a[p]==key)
+            return p;
+    }
+    return -1;
+}
+
+int main()
+{
+    int a[]= {1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(a)/sizeof(int);
+    int key = 11;
+    cout<<search(a,n,key);
+    return 0;
+}
+
+//Prog using Generic Programming.
+#include<iostream>
+using namespace std;
+
+template<typename T>
+int search(T arr[],int n, T key)
+{
+    for(int p=0;p<n;p++)
+    {
+        if(arr[p]==key)
+            return p;
+    }
+    return -1;
+}
+
+int main()
+{
+    float f[] = {1.2,2.5,3.5,4.8,5.4};
+    float key =2.55;
+    cout<<search(f,5,key);
+    return 0;
+}
+
